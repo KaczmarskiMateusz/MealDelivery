cda7b4ace78c38eeedceb66ba07adfecb5a880c8
Mateusz <58079969+KaczmarskiMateusz@users.noreply.github.com>
2025-09-23 16:44:13 +0200

    Start project

---
+++ src/main/java/pl/mealdelivery/MealDeliveryApplication.java
@@ -0,0 +1,13 @@
+package pl.mealdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mealdelivery/MealDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mealdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
