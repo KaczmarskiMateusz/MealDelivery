1a0725a08b8559f9c5c4afd1b1883c67b55475b7
mati <kaczmarskim95@gmail.com>
2025-10-13 21:00:31 +0200

    Add CompanyRequestequest

--- src/main/java/pl/mealdelivery/Model/CompanyRequest.java
+++ src/main/java/pl/mealdelivery/Model/CompanyRequest.java
@@ -1,4 +1,15 @@
 package pl.mealdelivery.Model;
 
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.UUID;
+
 public class CompanyRequest {
+
+    private UUID externalId;
+    private String name;
+    private String address;
+    private String phone;
+    private String email;
+
 }
